4ff6d401ed230268bd11b9bab95dd09144b8c5b6
szamacz <szamacz@o2.pl>
2019-02-11 18:22:25 +0100

    Initial comit

---
+++ Pages/CaseTakedaDownloadPage.cs
@@ -0,0 +1,32 @@
+﻿using Omada.Utils;
+using OpenQA.Selenium;
+using System.Configuration;
+
+namespace Omada.Pages
+{
+    public class CaseTakedaDownloadPage : OmadaPage
+    {
+        private IWebDriver _driver;
+
+        private By DownloadCustomerCase = By.CssSelector(".text__text a");
+
+        public CaseTakedaDownloadPage(IWebDriver driver) : base(driver)
+        {
+            _driver = driver;
+        }
+
+        public CaseTakedaDownloadPage GoTo()
+        {
+            helper.OpenPage(Constant.TakedaCasePageUrl);
+            return this;
+        }
+
+        public CaseTakedaDownloadPage DownloadFileAndVerify()
+        {
+            helper.WaitAndClick(DownloadCustomerCase);
+            helper.WaitForFileToDownload(ConfigurationManager.AppSettings["FileDownloadLocation"]
+                + Constant.TakedaCaseFileName, Constant.TakedaCaseFileSize);
+            return this;
+        }
+    }
+}
---
+++ Pages/NewsPage.cs
@@ -0,0 +1,24 @@
+﻿using Omada.Utils;
+using OpenQA.Selenium;
+
+namespace Omada.Pages
+{
+    public class NewsPage : OmadaPage
+    {
+        private IWebDriver _driver;
+
+        private By NewsList => By.CssSelector(".cases__heading");
+        
+
+        public NewsPage(IWebDriver driver) : base(driver)
+        {
+            _driver = driver;
+        }
+
+        public NewsPage VerifyNewsContainsArticle(string articleName)
+        {
+            helper.WaitForElementWithText(NewsList, articleName);
+            return this;
+        }
+    }
+}
